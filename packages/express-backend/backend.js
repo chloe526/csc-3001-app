29b8a2dde29174e685ac34503e3de65b14f817a7
chlow526 <2chloeee@gmail.com>
2026-09-12 01:36:26 -0700

    feat: delete user by id endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -82,3 +82,24 @@ app.post("/users", (req, res) => {
   addUser(userToAdd);
   res.send();
 });
+
+const deleteUserById = (id) => {
+  const index = users.users_list.findIndex((user) => user.id === id);
+
+  if (index === -1) {
+    return undefined;
+  }
+
+  const [deletedUser] = users.users_list.splice(index, 1);
+  return deletedUser;
+};
+
+app.delete("/users/:id", (req, res) => {
+  const deletedUser = deleteUserById(req.params.id);
+
+  if (deletedUser === undefined) {
+    return res.status(404).send("Resource not found.");
+  }
+
+  res.status(200).send(deletedUser);
+});
